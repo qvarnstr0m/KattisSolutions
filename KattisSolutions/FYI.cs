fed87e2c0ccec6439edadd847573143e14abe3db
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-04 10:19:00 +0200

    Add project files.

---
+++ KattisSolutions/FYI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KattisSolutions
+{
+    internal class FYI
+    {
+        static void FYISolution()
+        {
+            string input = Console.ReadLine();
+            if (input.Substring(0, 3) == "555")
+                Console.Write("1");
+            else
+                Console.Write("0");
+        }
+    }
+}
---
+++ KattisSolutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KattisSolutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
