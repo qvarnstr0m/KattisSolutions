0c0bd0d41dcea3699922d08102caebef75d300ef
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-04 20:07:15 +0200

    Tarifa problem solved

---
+++ KattisSolutions/tarifa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KattisSolutions
+{
+    internal class tarifa
+    {
+        int megaByte = int.Parse(Console.ReadLine());
+        int months = int.Parse(Console.ReadLine());
+        int leftOver = 0;
+        for (int i = 0; i < months; i++)
+        {
+            leftOver += megaByte - int.Parse(Console.ReadLine());
+        }
+        Console.Write(megaByte + leftOver);
+    }
+}
