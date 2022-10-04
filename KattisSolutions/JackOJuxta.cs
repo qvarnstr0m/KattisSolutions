7dcf5c072cd0128b517581a41b7694a0746d9930
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-04 20:50:57 +0200

    Jack-O'-Lantern Juxtaposition problem solved

---
+++ KattisSolutions/JackOJuxta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KattisSolutions
+{
+    internal class JackOJuxta
+    {
+        internal void JackOJuxtaSolution()
+        {
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+            int eyes = int.Parse(split[0]);
+            int nose = int.Parse(split[1]);
+            int mouth = int.Parse(split[2]);
+            Console.Write(eyes * nose * mouth);
+        }
+    }
+}
