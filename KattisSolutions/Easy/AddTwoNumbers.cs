7df4449ecaad5f9adad46f7c6f07dae1f772ce7e
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-05 15:17:57 +0200

    Add two numbers problem solved.

---
+++ KattisSolutions/Easy/AddTwoNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class AddTwoNumbers
+    {
+        internal void AddTwoNumbersSolutíon()
+        {
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+            int first = int.Parse(split[0]);
+            int second = int.Parse(split[1]);
+            Console.Write(first + second);
+        }
+    }
+}
