2e074e8fb593d5d12359eff4ec90b5055f9cbe29
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-05 14:28:32 +0200

    Chanukah challange solved.

---
+++ KattisSolutions/ChanukahChallenge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KattisSolutions
+{
+    internal class ChanukahChallenge
+    {
+        internal void ChanukahChallengeSolution()
+        {
+            int dataSets = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= dataSets; i++)
+            {
+                string line = Console.ReadLine();
+                string[] split = line.Split(new char[] { ' ' });
+                int[] arraySplit = Array.ConvertAll(split, int.Parse);
+                int candles = 0;
+
+                for (int j = 1; j <= arraySplit[1]; j++)
+                {
+                    candles += j + 1;
+                }
+
+                Console.WriteLine($"{i} {candles}");
+            }
+        }
+    }
+}
