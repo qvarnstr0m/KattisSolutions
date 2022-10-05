1661451816de89277979952bb8a1ef0464311138
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-05 11:28:26 +0200

    Triangle area problem solved

---
+++ KattisSolutions/TriangleArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KattisSolutions
+{
+    internal class TriangleArea
+    {
+        internal void TriangleAreaSolution()
+        {
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+            float cakeHeight = float.Parse(split[0]);
+            float cakeBase = float.Parse(split[1]);
+            Console.Write((cakeBase * cakeHeight) / 2);
+        }
+    }
+}
