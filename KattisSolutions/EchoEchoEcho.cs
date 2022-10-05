38579e16e34f908917483b4fd3397420c4716c6b
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-05 10:56:48 +0200

    Echo echo echo problem solved

---
+++ KattisSolutions/EchoEchoEcho.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KattisSolutions
+{
+    internal class EchoEchoEcho
+    {
+        internal void EchoSolution()
+        {
+            string word = Console.ReadLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"{word} ");
+            }
+        }
+    }
+}
