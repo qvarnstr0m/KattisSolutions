9baf7660366162e7f8840a86e4f179e1dd0116d6
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-05 13:35:55 +0200

    Cold-puter science problem solved

---
+++ KattisSolutions/Cold-puterScience.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KattisSolutions
+{
+    internal class Cold_puterScience
+    {
+        internal void Cold_puterScienceSolution()
+        {
+            int daysCollected = int.Parse(Console.ReadLine());
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' });
+            int[] splitInts = Array.ConvertAll(split, int.Parse);
+
+            int daysBelowZero = 0;
+
+            foreach (var item in splitInts)
+            {
+                if (item < 0) daysBelowZero++;
+            }
+
+            Console.Write(daysBelowZero);
+        }
+    }
+}
