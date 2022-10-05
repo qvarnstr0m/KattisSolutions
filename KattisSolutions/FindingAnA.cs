fd7e4bffa204b145b685cb0ab140dd598019cb76
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-05 11:35:04 +0200

    Finding an a problem solved.

---
+++ KattisSolutions/FindingAnA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KattisSolutions
+{
+    internal class FindingAnA
+    {
+        internal void FindingAnASolution()
+        {
+            string line = Console.ReadLine();
+            Console.Write(line.Substring(line.IndexOf('a')));
+        }
+    }
+}
