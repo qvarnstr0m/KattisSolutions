9a1614e80c19bc16bb2507c5c9b260505fd5f2de
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-05 11:09:55 +0200

    Which is greater problem solved

---
+++ KattisSolutions/WhichIsGreater.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KattisSolutions
+{
+    internal class WhichIsGreater
+    {
+        internal void WhichIsGreaterSolution()
+        {
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+            int first = int.Parse(split[0]);
+            int second = int.Parse(split[1]);
+            if (first > second) Console.Write("1");
+            else Console.Write("0");
+        }
+    }
+}
