32b2ade2977e1ec6d89f3030884e9cdd027ba377
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-05 15:08:22 +0200

    Autori problem solved.

---
+++ KattisSolutions/Easy/Autori.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace KattisSolutions.Easy
+{
+    internal class Autori
+    {
+        internal void AutoriSolution()
+        {
+            string line = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (i == 0) sb.Append(line[0]);
+                if (line[i] == '-') sb.Append(line[i + 1]);
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
