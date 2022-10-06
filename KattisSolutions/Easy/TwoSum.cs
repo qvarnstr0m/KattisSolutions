eb4547e573e80d92bbf0ca6a86d599c4f6c650f1
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-06 15:09:41 +0200

    Zamka and Two sum problems solved.

---
+++ KattisSolutions/Easy/TwoSum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class TwoSum
+    {
+        internal void TwoSumSolution()
+        {
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' });
+            Console.Write(int.Parse(split[0]) + int.Parse(split[1]));
+        }
+    }
+}
---
+++ KattisSolutions/Easy/Zamka.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace KattisSolutions.Easy
+{
+    internal class Zamka
+    {
+        internal void ZamkaSolution()
+        {
+            int low = int.Parse(Console.ReadLine());
+            int high = int.Parse(Console.ReadLine());
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int i = low; i <= high; i++)
+            {
+                if (i.ToString().Sum(c => c - '0') == sum)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+
+            for (int i = high; i >= low; i--)
+            {
+                if (i.ToString().Sum(c => c - '0') == sum)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+    }
+}
