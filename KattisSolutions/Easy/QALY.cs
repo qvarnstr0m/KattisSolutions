7a87b5ec32971f6c9b11b1f65c1f663d0ad382dd
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-10 21:27:49 +0200

    R2 and QALY problems solved.

---
+++ KattisSolutions/Easy/QALY.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class QALY
+    {
+        internal void QALYSolution()
+        {
+            int periods = int.Parse(Console.ReadLine());
+            decimal QalyTotal = 0m;
+
+            for (int i = 0; i < periods; i++)
+            {
+                string line = Console.ReadLine();
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                decimal qaly = decimal.Parse(split[0]);
+                decimal time = decimal.Parse(split[1]);
+                QalyTotal += qaly * time;
+            }
+
+            Console.Write(QalyTotal);
+        }
+    }
+}
---
+++ KattisSolutions/Easy/R2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KattisSolutions.Easy
+{
+    internal class R2
+    {
+        internal void R2Solution()
+        {
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+            int R1 = int.Parse(split[0]);
+            int S = int.Parse(split[1]);
+            Console.Write((2 * S) - R1);
+        }
+    }
+}
