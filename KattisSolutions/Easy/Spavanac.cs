b44f87614f47195ec7cbd9f03f6f444c907ba1c5
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-10 14:31:05 +0200

    Spavanac problem solved.

---
+++ KattisSolutions/Easy/Spavanac.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class Spavanac
+    {
+        internal void SpavanacSolution()
+        {
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+            int hour = int.Parse(split[0]);
+            int minute = int.Parse(split[1]);
+
+            minute -= 45;
+            if (minute >= 0)
+                Console.Write($"{hour} {minute}");
+            else
+            {
+                hour -= 1;
+                if (hour < 0) hour += 24;
+                minute += 60;
+                Console.Write($"{hour} {minute}");
+            }
+        }
+    }
+}
