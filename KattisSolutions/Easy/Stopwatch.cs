89823ff042c9438dd52f0d5703dfd98a7111e391
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-10 14:08:44 +0200

    Stopwatch problem solved.

---
+++ KattisSolutions/Easy/Stopwatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class Stopwatch
+    {
+        public void StopClassSolution()
+        {
+            int timesPressed = int.Parse(Console.ReadLine());
+
+            if (timesPressed % 2 != 0)
+                Console.Write("still running");
+            else
+            {
+                int timerSum = 0;
+                int[] timerArray = new int[timesPressed];
+                for (int i = 0; i < timesPressed; i++)
+                {
+                    timerArray[i] = int.Parse(Console.ReadLine());
+                }
+
+                for (int i = 0; i < timerArray.Length - 1; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        timerSum += timerArray[i + 1] - timerArray[i];
+                    }
+                }
+
+                Console.Write(timerSum);
+            }
+        }
+    }
+}
---
+++ KattisSolutions/Easy/TransitWoes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KattisSolutions.Easy
+{
+    internal class TransitWoes
+    {
+        internal void TransitWoesSolution()
+        {
+            string[] line1 = Console.ReadLine().Split(' ');
+            int startTime = int.Parse(line1[0]);
+            int schoolTime = int.Parse(line1[1]);
+            int bussRides = int.Parse(line1[2]);
+
+            string[] line2 = Console.ReadLine().Split(' ');
+            int walkTime = 0;
+            foreach (var item in line2)
+            {
+                walkTime += int.Parse(item);
+            }
+
+            string[] line3 = Console.ReadLine().Split(' ');
+            int rideTime = 0;
+            foreach (var item in line3)
+            {
+                rideTime += int.Parse(item);
+            }
+
+            string[] line4 = Console.ReadLine().Split(' ');
+            int waitTime = 0;
+            for (int i = 0; i < line4.Length; i++)
+            {
+
+            }
+            if (startTime + walkTime + rideTime + waitTime > schoolTime)
+                Console.Write("no");
+            else
+                Console.Write("yes");
+        }
+    }
+}
