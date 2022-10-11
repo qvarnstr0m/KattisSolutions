55b62725ef65bf89558ed6d623794f4158c26b3a
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-11 12:04:55 +0200

    Pet problem solved.

---
+++ KattisSolutions/Easy/Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class Pet
+    {
+        internal void PetSolution()
+        {
+            int topScore = 0;
+            int winner = 0;
+            int total = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                string line = Console.ReadLine();
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                int firstScore = int.Parse(split[0]);
+                int secondScore = int.Parse(split[1]);
+                int thirdScore = int.Parse(split[2]);
+                int fourthScore = int.Parse(split[3]);
+                total = firstScore + secondScore + thirdScore + fourthScore;
+                if (total > topScore)
+                {
+                    topScore = total;
+                    winner = i;
+                }
+            }
+            Console.Write($"{winner} {topScore}");
+        }
+    }
+}
