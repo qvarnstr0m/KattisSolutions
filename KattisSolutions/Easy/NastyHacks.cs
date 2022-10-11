60e020cf05f7339368695acccb1c4275d49d2e25
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-11 13:38:48 +0200

    Nasty hacks problem solved.

---
+++ KattisSolutions/Easy/NastyHacks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class NastyHacks
+    {
+        internal void NastyHacksSolution()
+        {
+            int iterations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < iterations; i++)
+            {
+                string line = Console.ReadLine();
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                int revenueWithoutAds = int.Parse(split[0]);
+                int revenueWithAds = int.Parse(split[1]) - int.Parse(split[2]);
+
+                if (revenueWithAds == revenueWithoutAds) Console.WriteLine("does not matter");
+                else if (revenueWithAds > revenueWithoutAds) Console.WriteLine("advertise");
+                else if (revenueWithoutAds > revenueWithAds) Console.WriteLine("do not advertise");
+            }
+        }
+    }
+}
