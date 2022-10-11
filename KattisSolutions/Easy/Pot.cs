eb45c131cadde35340f599eea19852917e057438
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-11 11:01:19 +0200

    Pot problem solved.

---
+++ KattisSolutions/Easy/Pot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KattisSolutions.Easy
+{
+    internal class Pot
+    {
+        internal void PotSolution()
+        {
+            int iterations = int.Parse(Console.ReadLine());
+            double total = 0;
+            for (int i = 0; i < iterations; i++)
+            {
+                string line = Console.ReadLine();
+                double baseNumber = double.Parse(line.Substring(0, line.Length - 1));
+                double powNumber = double.Parse(line.Substring(line.Length - 1));
+                total += Math.Pow(baseNumber, powNumber);
+            }
+            Console.Write(total);
+        }
+    }
+}
