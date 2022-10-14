1c10aa129c6341d625366ec7e653d0d9205aa364
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-14 11:00:45 +0200

    A different problem solved.

---
+++ KattisSolutions/Medium/ADifferentProblem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KattisSolutions.Medium
+{
+    internal class ADifferentProblem
+    {
+        internal void ADifferentProblemSolution()
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                long a = Int64.Parse(split[0]);
+                long b = Int64.Parse(split[1]);
+                Console.WriteLine(Math.Abs(a - b));
+            }
+        }
+    }
+}
