88cf1e1245fd24d6e9739ead2c28cf919536388e
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-14 12:14:12 +0200

    CD problem solved.
    Only first test case, second gets runtime error.

---
+++ KattisSolutions/Medium/CD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KattisSolutions.Medium
+{
+    internal class CD
+    {
+        internal void CDSolution()
+        {
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+            int jackCDs = int.Parse(split[0]);
+
+            var jackList = new List<long>();
+
+            int canBeSold = 0;
+
+            for (int i = 0; i < jackCDs; i++)
+            {
+                jackList.Add(long.Parse(Console.ReadLine()));
+            }
+
+            string jillInput;
+            while ((jillInput = Console.ReadLine()) != "0 0")
+            {
+                long cd = long.Parse(jillInput);
+                if (jackList.Contains(cd))
+                {
+                    canBeSold++;
+                }
+            }
+
+            Console.Write(canBeSold);
+        }
+    }
+}
