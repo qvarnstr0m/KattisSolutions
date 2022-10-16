6a26122ac67c22c8796fa76d8d5c7f0a38b6e32f
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-16 22:25:37 +0200

    Apaxiaaaaaaaaaaaansproblem solved.

---
+++ KattisSolutions/Easy/Apaxiaaaaaaaaaaaans.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace KattisSolutions.Easy
+{
+    internal class Apaxiaaaaaaaaaaaans
+    {
+        internal void ApaxiansSolution()
+        {
+            var sb = new StringBuilder();
+            string line = Console.ReadLine();
+            sb.Append(line[0]);
+
+            for (int i = 1; i < line.Length; i++)
+            {
+                if (line[i] != line[i - 1])
+                {
+                    sb.Append(line[i]);
+                }
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
