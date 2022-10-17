f600616ad95af2b913940e8e9d245446bc9c4a39
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-17 13:49:58 +0200

    Take two stones and Electrical outlets problems solved.

---
+++ KattisSolutions/Easy/ElectricalOutlets.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class ElectricalOutlets
+    {
+        internal void ElectricalOutletsSolution()
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            int totalOutlets = 1;
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string line = Console.ReadLine();
+                string[] split = line.Split(new char[] { ' ' });
+                int iterations = int.Parse(split[0]);
+                for (int j = 1; j <= iterations; j++)
+                {
+                    totalOutlets--;
+                    totalOutlets += int.Parse(split[j]);
+                }
+                Console.WriteLine(totalOutlets);
+                totalOutlets = 1;
+            }
+        }
+    }
+}
---
+++ KattisSolutions/Easy/TakeTwoStones.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class TakeTwoStones
+    {
+        internal void TakeTwoStonesSolution()
+        {
+            int stones = int.Parse(Console.ReadLine());
+            if (stones % 2 == 0) Console.Write("Bob");
+            else Console.WriteLine("Alice");
+        }
+    }
+}
