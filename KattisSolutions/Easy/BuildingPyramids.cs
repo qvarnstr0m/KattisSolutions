81020d1526ba34fe87e53e84ccf53755ed4b4db1
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-17 12:33:33 +0200

    Building pyramids and Oddities problem solved.

---
+++ KattisSolutions/Easy/BuildingPyramids.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class BuildingPyramids
+    {
+        internal void BuildingPyramidsSolution()
+        {
+            double blocks = double.Parse(Console.ReadLine());
+            int height = 0;
+            double blocksInLayer = 1;
+            int iterator = 1;
+            while (blocks >= 0)
+            {
+                blocksInLayer = Math.Pow(iterator, 2);
+                blocks -= blocksInLayer;
+                iterator += 2;
+
+                if (blocks >= 0) height++;
+            }
+
+            Console.Write(height);
+        }
+    }
+}
---
+++ KattisSolutions/Easy/Oddities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class Oddities
+    {
+        internal void OdditiesSolution()
+        {
+            int iterations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < iterations; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0) Console.WriteLine($"{number} is even");
+                else Console.WriteLine($"{number} is odd");
+            }
+        }
+    }
+}
