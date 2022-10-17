9f6b0a0cfcd4c99c0f0e28cd80df252c88b2b12b
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-17 12:05:41 +0200

    Shattered cake and Solving for carrots problems solved.

---
+++ KattisSolutions/Easy/ShatteredCake.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class ShatteredCake
+    {
+        internal void ShatteredCakeSolution()
+        {
+            int totalWidth = int.Parse(Console.ReadLine());
+            int pieces = int.Parse(Console.ReadLine());
+            int totalArea = 0;
+
+            for (int i = 0; i < pieces; i++)
+            {
+                string line = Console.ReadLine();
+                string[] split = line.Split(new char[] { ' ' });
+                int width = int.Parse(split[0]);
+                int height = int.Parse(split[1]);
+                totalArea += width * height;
+            }
+
+            Console.Write(totalArea / totalWidth);
+        }
+    }
+}
---
+++ KattisSolutions/Easy/SolvingForCarrots.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KattisSolutions.Easy
+{
+    internal class SolvingForCarrots
+    {
+        internal void SolvingForCarrotsSolution()
+        {
+            string line = Console.ReadLine();
+            string[] split = line.Split(new char[] { ' ' });
+            Console.Write(split[1]);
+        }
+    }
+}
