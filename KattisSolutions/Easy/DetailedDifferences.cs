9783ffcdf4edaecd387aebcb3ce487cf9fd9b744
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-10-28 12:14:34 +0200

    Detailed differences and Dice Cup problems solved.

---
+++ KattisSolutions/Easy/DetailedDifferences.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace KattisSolutions.Easy
+{
+    internal class DetailedDifferences
+    {
+        internal void DetailedDifferencesSolution()
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+                string line1 = Console.ReadLine();
+                string line2 = Console.ReadLine();
+                StringBuilder output = new StringBuilder();
+                for (int j = 0; j < line1.Length; j++)
+                {
+                    if (line1[j] == line2[j]) output.Append('.');
+                    else output.Append('*');
+                }
+                Console.WriteLine(line1);
+                Console.WriteLine(line2);
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ KattisSolutions/Easy/DiceCup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KattisSolutions.Easy
+{
+    internal class DiceCup
+    {
+        internal void DiceCupSolution()
+        {
+            int[] split = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int firstDice = split[0];
+            int secondDice = split[1];
+            List<int> totals = new List<int>();
+
+            for (int i = 1; i <= firstDice; i++)
+            {
+                for (int j = 1; j <= secondDice; j++)
+                {
+                    totals.Add(i + j);
+                }
+            }
+
+            Dictionary<int, int> totalsDict = new Dictionary<int, int>();
+
+            foreach (var item in totals)
+            {
+                totalsDict[item] = (!totalsDict.ContainsKey(item)) ? totalsDict[item] = 1 : totalsDict[item] = totalsDict[item] + 1;
+            }
+
+            int mostOccuring = totalsDict.Values.Max();
+
+            foreach (var item in totalsDict)
+            {
+                if (item.Value == mostOccuring) Console.WriteLine(item.Key);
+            }
+        }
+    }
+}
