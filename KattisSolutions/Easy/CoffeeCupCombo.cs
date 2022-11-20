4e40c65388c8591c7bde4474a29c08b40325d34c
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-11-20 20:46:27 +0100

    Coffee Cup Combo and QuickEstimates solved.

---
+++ KattisSolutions/Easy/CoffeeCupCombo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KattisSolutions.Easy
+{
+    internal class CoffeeCupCombo
+    {
+        internal void CoffeeCupComboSolution()
+        {
+            Console.ReadLine();
+            string line = Console.ReadLine();
+
+            int awake = 0;
+            int carry = 0;
+
+            foreach (char c in line)
+            {
+                if (c == '1')
+                {
+                    awake++;
+                    carry = 2;
+                }
+                else
+                {
+                    if (carry > 0)
+                    {
+                        awake++;
+                        carry--;
+                    }
+                }
+            }
+            Console.Write(awake);
+        }
+    }
+}
---
+++ KattisSolutions/Easy/QuickEstimates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KattisSolutions.Easy
+{
+    internal class QuickEstimates
+    {
+        internal void QuickEstimatesSolution()
+        {
+            int iterations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < iterations; i++)
+            {
+                string line = Console.ReadLine();
+                int digits = 0;
+                foreach (char c in line)
+                {
+                    digits++;
+                }
+                Console.WriteLine(digits);
+            }
+        }
+    }
+}
