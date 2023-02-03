16e8138440bd88b6e53ffbaa78cb433eb3cb1388
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-02-03 15:24:39 +0100

    The last problem solved.

---
+++ KattisSolutions/Easy/TheLastProblem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KattisSolutions.Easy
+{
+    internal class TheLastProblem
+    {
+        internal void TheLastProblemSolution()
+        {
+            Console.Write($"Thank you, {Console.ReadLine()}, and farewell!");
+        }
+    }
+}
