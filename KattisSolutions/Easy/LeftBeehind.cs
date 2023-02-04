a1c1a4bccb6840dd9b083c2b2ea38c0f97edf630
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-02-04 22:11:26 +0100

    Left beehind solved.

---
+++ KattisSolutions/Easy/LeftBeehind.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KattisSolutions.Easy
+{
+    internal class LeftBeehind
+    {
+        internal static void LeftBeehindSolution()
+        {
+            while (true)
+            {
+                int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                int first = input[0];
+                int second = input[1];
+
+                if (first == 0 && second == 0) break;
+                if (first + second == 13) Console.WriteLine("Never speak again.");
+                else if (first == second) Console.WriteLine("Undecided.");
+                else if (first < second) Console.WriteLine("Left beehind.");
+                else if (first > second) Console.WriteLine("To the convention.");
+            }
+        }
+    }
+}
